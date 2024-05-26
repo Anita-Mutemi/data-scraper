a58957b58c9bb9c4c19c2716bb4e56f9db96dda0
Anita-Mutemi <anitajehu13@gmail.com>
2024-05-26 10:58:21 +0300

    Update db

--- models.py
+++ models.py
@@ -4,31 +4,22 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String, nullable=False, unique=True)
-
-    posts = relationship('Post', back_populates='user')
-
-class Post(Base):
-    __tablename__ = 'posts'
+class Postowner(Base):
+    __tablename__ = 'postsowners'
 
     id = Column(Integer, primary_key=True)
     post_id = Column(String, nullable=False, unique=True)
-    user_id = Column(String, ForeignKey('users.user_id'), nullable=False)
+    user_id = Column(String, nullable=False, unique=True)
 
-    user = relationship('User', back_populates='posts')
     likers = relationship('Liker', back_populates='post')
 
 class Liker(Base):
     __tablename__ = 'likers'
 
     id = Column(Integer, primary_key=True)
-    liker_id = Column(String, nullable=False)
-    name = Column(String, nullable=False)
-    title = Column(String)
-    post_id = Column(String, ForeignKey('posts.post_id'), nullable=False)
+    liker_id = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False, unique=True)
+    title = Column(String, nullable=False, unique=True)
+    post_id = Column(String, ForeignKey('Postowner.post_id'), nullable=False, unique=True)
 
-    post = relationship('Post', back_populates='likers')
+    post = relationship('Postowner', back_populates='likers')
